133bfa6bdb077daa127a50a37dc599f429a0da10
longnt <longnt1@kaopiz.com>
2020-02-11 14:20:13 +0700

    Updating animation data as parametter

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,15 @@ let cat;
 setup();
 
 function setup(){
-    cat = new Kultie.Entity(catRes, "cat");
+    cat = new Kultie.Entity(catRes, "cat",{
+        "idle": [ 0, 1, 2, 3 ],
+        "walk": [ 4, 5, 6, 7, 8, 9, 10, 11 ],
+        "start_jump": [ 12, 13 ],
+        "air": [ 14, 15 ],
+        "end_jump": [ 16, 17, 18, 19 ],
+        "ground_punch": [ 74, 75, 76, 77, 78, 79, 80, 81, 82 ],
+        "ground_kick": [ 83, 84, 84, 84, 84, 84, 86, 88, 84, 91, 92, 84, 97, 98, 84, 86, 88, 84, 91, 92, 84, 97, 98, 99, 99, 100 ]
+    });
     app.stage.addChild(cat);
 
     app.ticker.add(update);
--- scripts/sprite.js
+++ scripts/sprite.js
@@ -9,20 +9,12 @@ let Application = PIXI.Application,
 var Kultie = Kultie || {};
 
 Kultie.Entity = class extends PIXI.Sprite{
-    constructor(source, prefix){
+    constructor(source, prefix, animData){
         super();
         loader.add(source).load(this.spriteReady.bind(this));
         this._prefix = prefix;
         this._isReady = false;
-        this._animData = {
-            "idle": [ 0, 1, 2, 3 ],
-            "walk": [ 4, 5, 6, 7, 8, 9, 10, 11 ],
-            "start_jump": [ 12, 13 ],
-            "air": [ 14, 15 ],
-            "end_jump": [ 16, 17, 18, 19 ],
-            "ground_punch": [ 74, 75, 76, 77, 78, 79, 80, 81, 82 ],
-            "ground_kick": [ 83, 84, 84, 84, 84, 84, 86, 88, 84, 91, 92, 84, 97, 98, 84, 86, 88, 84, 91, 92, 84, 97, 98, 99, 99, 100 ]
-        }
+        this._animData = animData;
         this._sheet = this._animData["idle"];
         this._animation = new Kultie.AnimationSystem(this._sheet.length, true, 12);
     }
@@ -30,7 +22,7 @@ Kultie.Entity = class extends PIXI.Sprite{
     update(){
         if(this._isReady){
             this._animation.update();
-            this.setTexture(this.getSprite());
+            this.texture = this.getSprite();
         }        
     }
 
